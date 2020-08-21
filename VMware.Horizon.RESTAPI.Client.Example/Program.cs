f987363fbafa18113ce1a36ca92c9bdf7499d245
Andrew Morgan <andrew@andrewmorgan.ie>
2020-08-21 16:29:06 +0100

    1.0

--- VMware.Horizon.RESTAPI.Client.Example/Program.cs
+++ VMware.Horizon.RESTAPI.Client.Example/Program.cs
@@ -10,13 +10,19 @@ namespace VMware.Horizon.RESTAPI.Client.Example
     {
         static void Main(string[] args)
         {
-            HorizonRESTClient client = new HorizonRESTClient("https://yourconnectionserver/rest");
+            HorizonRESTClient client = new HorizonRESTClient("https://connectionserver.domain.local/rest");
 
             try
             {
                 client.Logon("username", "password", "domain");
+                
+                // list all farms health
                 var farms = client.Monitoring.ListFarmMonitors();
+
+                // list all rdsh servers health
                 var RDSHServers = client.Monitoring.ListRDSServerMonitors();
+
+                //list all connection servers health
                 var ConnectionServers = client.Monitoring.ListConnectionServerMonitors();
 
 
@@ -24,8 +30,11 @@ namespace VMware.Horizon.RESTAPI.Client.Example
                 client.TryRefreshSession();
 
                 var pools = client.Inventory.ListDesktopPools();
-
+                
+                // get information about the first pool
                 var pool = client.Inventory.GetDesktopPool(pools[0].Id);
+
+                // pull active sessions
                 var sessions = client.Inventory.ListSessionInfo();
 
                 // log out when complete
